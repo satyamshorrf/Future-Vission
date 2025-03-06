0dd29fca80d54e16149ee51c1223afbe06adc6ad
satyamshorrf <satyamshorrf@gmail.com>
2025-03-07 02:06:10 +0530

    name edit header

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Video Player App",
+  title: "Future-Vision",
   description: "A Next.js application with auto-playing videos",
    
 }
